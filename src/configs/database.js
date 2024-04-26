c5b10d22788990a59e2dae7b2d8d302e03d70eac
anon-kae <anon.ksl42@gmail.com>
2024-04-26 19:49:57 +0700

    feat: add config

--- src/configs/database.js
+++ src/configs/database.js
@@ -4,7 +4,7 @@ const config = {
   password: process.env.DB_PASS, // Database password
   server: process.env.DB_HOST, // Server IP address
   database: process.env.DB_NAME, // Database name
-  prot: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT) || 1433, // Database port
   options: {
     encrypt: false, // Disable encryption
     instancename: process.env.DB_INSTANCE_NAME
