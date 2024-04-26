9fbeb988612c5129a82073702a5fb080cbeacd03
anon-kae <anon.ksl42@gmail.com>
2024-04-26 16:46:39 +0700

    feat: rename file to sftp

--- src/configs/sftpConnect.js
+++
@@ -1,8 +0,0 @@
-const configSFTP = {
-    'host': process.env.SFTP_HOST,
-    'port': process.env.SFTP_PORT,
-    'username': process.env.SFTP_USERNAME,
-    'password': process.env.SFTP_PASSWORD
-}
-
-module.exports = configSFTP
