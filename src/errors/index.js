feeef85d9c48b4d489ae8b3c62c6f2c4be7eb14a
anon-kae <anon.ksl42@gmail.com>
2024-04-26 19:49:18 +0700

    feat: add error handler

---
+++ src/errors/index.js
@@ -0,0 +1,11 @@
+const AuthenticationError = require('./AuthenticationError');
+const PermissionError = require('./PermissionError');
+const ResourceNotFoundError = require('./ResourceNotFoundError');
+const ValidationError = require('./ValidationError');
+
+module.exports = {
+  AuthenticationError,
+  PermissionError,
+  ResourceNotFoundError,
+  ValidationError
+};
