797809210ac7ec228aeb1bcb4421eda244829ee8
anon-kae <anon.ksl42@gmail.com>
2024-04-26 19:49:33 +0700

    feat: add controller

---
+++ src/controllers/RootController.js
@@ -0,0 +1,21 @@
+const joi = require('joi');
+const { runValidation } = require('../helpers/runValidation');
+const { success } = require('../helpers/response');
+const Logger = require('../helpers/Logger');
+
+exports.getApiIdentity = async (req, res, next) => {
+  try {
+    const request = await runValidation({}, joi.object());
+
+    const result = 'TCAP Api to meta.';
+
+    Logger.info(`Successfully get API identity: ${result}`, {
+      result,
+      request,
+    });
+
+    success(res, { status: 'SUCCESS', result });
+  } catch (error) {
+    next(error);
+  }
+};
